c87060388a5b86c12c0c8a9131939c303ed6da84
Paweł Goc <pawel.goc@solutionsby.pl>
2023-07-18 18:41:21 +0200

    Header mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Header } from "./components/header/header";
 
 function App() {
-  return <div>Test</div>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/button/CustomButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type CustomButtonProps = {
+  href: string;
+  className: string;
+  children: React.ReactNode; // Lepsze używanie ogólnego typu React.ReactNode
+};
+
+export const CustomButton = ({
+  href,
+  children,
+  className,
+}: CustomButtonProps) => {
+  return (
+    <a href={href} className={`custom-button ${className}`}>
+      {children}
+    </a>
+  );
+};
