2352818f4b8b623a90f02d8f89c797a8775b68c4
Paweł Goc <pawel.goc@solutionsby.pl>
2023-08-18 11:24:17 +0200

    create slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import "./slider.scss";
 
-export const Slider = () => {
-  return <div className="car-slider-wrapper"></div>;
+export const Slider = ({ slides }: { slides: Array }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPreviuse = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  return (
+    <div className="car-slider-wrapper">
+      <div className="left-arrow slider-arrow" onClick={goToPreviuse}>
+        ❰
+      </div>
+      <div className="right-arrow slider-arrow " onClick={goToNext}>
+        ❱
+      </div>
+      <div
+        className="car-photo"
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+    </div>
+  );
 };
